bbcd35f36f167c100483613370dc2b80b29e0c48
Narhiko ARASAKI <naruhiko.arasaki.21@shiftinc.jp>
2020-10-15 00:26:20 +0000

    リハーサルを受けて修正

--- src/exercise_02/functions.js
+++
@@ -1,6 +0,0 @@
-export const getPrice = (itemId) => {
-  if (itemId <= 2) return itemId * 100;
-  if (itemId <= 5) return itemId * 200;
-  if (itemId <= 10) return itemId * 500;
-  return 5000 + itemId * 10;
-};
