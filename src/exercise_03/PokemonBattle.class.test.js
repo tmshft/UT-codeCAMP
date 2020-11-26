c42044e1ad5de8f11f001dd41efa24cae02930b3
Narhiko ARASAKI <naruhiko.arasaki.21@shiftinc.jp>
2020-11-26 18:19:01 +0900

    lint errorの解消

--- src/exercise_03/PokemonBattle.class.test.js
+++ src/exercise_03/PokemonBattle.class.test.js
@@ -1,5 +1,7 @@
 import PokemonBattle from './PokemonBattle.class';
 
+/* global expect, describe, test */
+
 /**
  * describe()の第1引数は、第2引数の処理をdescribe（説明）するために使います。
  * describe()の第2引数は、実行される一連の処理です。
@@ -11,14 +13,14 @@ import PokemonBattle from './PokemonBattle.class';
  *
  * このようにdescribeを適切に用いることで、テストコードを構造化することができ、見通しやすくなります。
  */
-describe("PokemonBattleクラスのテスト", () => {
-  describe("正常系", () => {
+describe('PokemonBattleクラスのテスト', () => {
+  describe('正常系', () => {
     /**
      * test()の第1引数は、テスト結果時にタイトルとして使われます。
      * test()の第2引数は、テストを走らせたときに実行される処理です。
      * なので、第2引数に「テスト手順の実行」と「期待値」と「その2つの比較」を記述します。
      */
-    test("正常系のサンプル1", () => {
+    test('正常系のサンプル1', () => {
       // テスト手順の実行
       const output = new PokemonBattle({
         level: 25,
@@ -38,7 +40,7 @@ describe("PokemonBattleクラスのテスト", () => {
      * 上記はわかりやすく書きました
      * 一時変数の定義を端折って以下のように書いてもいいです。
      */
-    test("正常系のサンプル2", () => {
+    test('正常系のサンプル2', () => {
       expect(new PokemonBattle({
         level: 25,
         power: 40,
@@ -48,20 +50,20 @@ describe("PokemonBattleクラスのテスト", () => {
     });
   });
 
-  describe("異常系", () => {
+  describe('異常系', () => {
     /**
      * 異常系は、expectの第一引数に関数を書きます。
      * 関数内で、異常が生じる操作を実行し、
      * 外側でtoThrow()を用いて例外を比較します。
      */
-    test("異常系のサンプル", () => {
+    test('異常系のサンプル', () => {
       expect(() => {
         // 異常操作(PokemonBattleに初期値が与えられていない)を記述
-        new PokemonBattle();
+        const pb = new PokemonBattle();
+        pb.baseDamage();
       }).toThrow("Cannot read property 'level' of undefined");
     });
   });
 
   // TODO describe()とtest()を用いてテストコードを記述してください
 });
-
