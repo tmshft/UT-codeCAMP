32e8d92a7feec678f5c8bcc3825c5933522501a6
Narhiko ARASAKI <naruhiko.arasaki.21@shiftinc.jp>
2020-12-02 23:15:49 +0900

    レビュー指摘事項を修正

--- src/exercise_03/PokemonBattle.class.js
+++ src/exercise_03/PokemonBattle.class.js
@@ -26,8 +26,8 @@ class PokemonBattle {
     // ぼうぎょ 整数型 1~
     this.defense = stats.defense;
 
-    /*******ここからは上級者用の変数 *******/
-    // こうげきするポケモンと天気の相性 
+    // ここから下は、上級者用の変数
+    // こうげきするポケモンと天気の相性
     this.weatherMatch = stats.weatherMatch ? stats.weatherMatch : WEATHER_MATCH.NORMAL;
     // こうげきするポケモンとわざタイプが一致しているかどうか
     this.moveTypeMatchFlag = stats.moveTypeMatchFlag ? stats.moveTypeMatchFlag : false;
@@ -42,12 +42,12 @@ class PokemonBattle {
    * テスト対象: 初心者向け
    *
    * 仕様
-   * ダメージ = [[[Lv * 0.4 + 2] * 威力 * こうげき / ぼうぎょ] / 50 + 2]  
+   * ダメージ = [[[Lv * 0.4 + 2] * 威力 * こうげき / ぼうぎょ] / 50 + 2]
    * []は切り捨て
    * @return number
    */
   baseDamage = () => Math.floor(
-    Math.floor((this.level * 0.4 + 2) * this.power * this.attack / this.defense) / 50 + 2,
+    Math.floor(((this.level * 0.4 + 2) * this.power * this.attack) / this.defense) / 50 + 2,
   );
 
   /**
@@ -74,24 +74,26 @@ class PokemonBattle {
   finalDamage = () => {
     let result = this.baseDamage();
     // 弱化天気の場合は威力が1/2になる
-    if (this.weatherMatch === WEATHER_MATCH.WEAKENED) result *= 1/2;
+    if (this.weatherMatch === WEATHER_MATCH.WEAKENED) result *= 1 / 2;
     // 強化天気の場合は威力が3/2になる
-    if (this.weatherMatch === WEATHER_MATCH.REINFORCED) result *= 3/2;
+    if (this.weatherMatch === WEATHER_MATCH.REINFORCED) result *= 3 / 2;
     // わざタイプ一致の場合は威力が3/2になる
-    if (this.moveTypeMatchFlag) result *= 3/2;
+    if (this.moveTypeMatchFlag) result *= 3 / 2;
     // やけどの場合は威力が1/2になる
-    if (this.burnFlag) result *= 1/2;
+    if (this.burnFlag) result *= 1 / 2;
     // タイプ相性によって威力が変化する
+    // こうかはばつぐんだ -> 2倍
+    // こうかはいまひとつだ -> 0.5倍
+    // こうかがないみたいだ -> 0倍
     if (this.typePattern === TYPE_PATTERN.KOUKAHA_BATSUGUNDA) {
-      result *= 2; 
+      result *= 2;
     } else if (this.typePattern === TYPE_PATTERN.KOUKAHA_IMAHITOTSUNOYOUDA) {
-      result *= 1/2; 
+      result *= 1 / 2;
     } else {
-      result *= 0; 
+      result *= 0;
     }
     return Math.floor(result);
   };
-
 }
 
 export default PokemonBattle;
