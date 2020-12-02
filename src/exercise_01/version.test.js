1d1ece15b2573c9ba526602a3bfa397ba595725a
Tomoo Morikawa <tomoo.morikawa.84@shiftinc.jp>
2020-12-02 23:49:51 +0900

    disable old exercise test

--- src/exercise_01/version.test.js
+++ src/exercise_01/version.test.js
@@ -1,21 +1,21 @@
-/*
- * UT-codeCAMP
- * ワーク用テストコード
- *
- * Javaのバージョニング
- */
-import { Version } from "./version";
+// /*
+//  * UT-codeCAMP
+//  * ワーク用テストコード
+//  *
+//  * Javaのバージョニング
+//  */
+// import { Version } from "./version";
 
-describe("abnormal cases", () => {
-  test("<TODO please describe your test scope>", () => {
-    expect(() => new Version("")).toThrow(Error);
-    // TODO テストコードを実装してください
-  });
-});
+// describe("abnormal cases", () => {
+//   test("<TODO please describe your test scope>", () => {
+//     expect(() => new Version("")).toThrow(Error);
+//     // TODO テストコードを実装してください
+//   });
+// });
 
-describe("normal cases", () => {
-  test("<TODO please describe your test scope>", () => {
-    expect(new Version("11.2.3.4").feature).toEqual(11);
-    // TODO テストコードを実装してください
-  });
-});
+// describe("normal cases", () => {
+//   test("<TODO please describe your test scope>", () => {
+//     expect(new Version("11.2.3.4").feature).toEqual(11);
+//     // TODO テストコードを実装してください
+//   });
+// });
--- src/exercise_02/Cart.class.test.js
+++ src/exercise_02/Cart.class.test.js
@@ -1,30 +1,30 @@
-import Cart from "./Cart.class";
+// import Cart from "./Cart.class";
 
-describe("Example", () => {
-  test("<正常系>", () => {
-    // 操作を記述
-    const cart = new Cart();
-    // 商品をカートに追加
-    cart.setItem({ id: 1, quantity: 1 });
-    // 数量を2に変更
-    cart.setItem({ id: 1, quantity: 2 });
+// describe("Example", () => {
+//   test("<正常系>", () => {
+//     // 操作を記述
+//     const cart = new Cart();
+//     // 商品をカートに追加
+//     cart.setItem({ id: 1, quantity: 1 });
+//     // 数量を2に変更
+//     cart.setItem({ id: 1, quantity: 2 });
 
-    // 出力を定義
-    const output = cart.getItemList();
-    // 期待値を定義
-    const expected = new Map([[1, { id: 1, quantity: 2 }]]);
-    // 出力と期待値が合っているか確認
-    expect(output).toEqual(expected);
-  });
-  test("<異常系>", () => {
-    // 異常操作時に例外が投げられることを確認
-    expect(() => {
-      // 異常操作を記述
-      const cart = new Cart();
-      // マイナス個の商品をカートに追加
-      cart.setItem({ id: 1, quantity: -1 });
-    }).toThrow("quantity is non-negative integer");
-  });
-});
+//     // 出力を定義
+//     const output = cart.getItemList();
+//     // 期待値を定義
+//     const expected = new Map([[1, { id: 1, quantity: 2 }]]);
+//     // 出力と期待値が合っているか確認
+//     expect(output).toEqual(expected);
+//   });
+//   test("<異常系>", () => {
+//     // 異常操作時に例外が投げられることを確認
+//     expect(() => {
+//       // 異常操作を記述
+//       const cart = new Cart();
+//       // マイナス個の商品をカートに追加
+//       cart.setItem({ id: 1, quantity: -1 });
+//     }).toThrow("quantity is non-negative integer");
+//   });
+// });
 
-// TODO 以下にテストコードを実装してください
+// // TODO 以下にテストコードを実装してください
